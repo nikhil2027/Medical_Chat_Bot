12ff51d356cd30318684c4111737109334dc7993
nikhil kumar <nikhilkumar02102005@gmail.com>
2026-02-13 22:31:32 +0530

    Files structure Done

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir src
+mkdir research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files are created successfully"
